72e2ccbc5615b599ccf4b66be5662d478f86c5d2
Shaun Thomas <thomas@innoscale.de>
2016-08-03 21:37:31 +0200

    Setup npm

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@
  * Created by shaunthomas on 03/08/16.
  */
 var DirContentRetriever = require('../dir-content-retriever.js');
-var finalResult = DirContentRetriever.displayContent('/Users/shaunthomas/git2/DirContentRetriever/test/testFolder/foo');
+var finalResult = DirContentRetriever.displayContent('/Users/shaunthomas/git2/DirContentRetriever/test/testFolder/foo/');
 
 console.log("=== Displaying final result ===");
 console.log(finalResult);
